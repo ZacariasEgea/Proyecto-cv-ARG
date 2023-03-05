dcc66ac9c8e6743ca8a81e6b86e6230c5303fd23
ZacariasEgea <124217658+ZacariasEgea@users.noreply.github.com>
2023-03-05 11:27:24 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,13 @@
+
+
+document.getElementById("localidad").onclick = function() {
+     document.getElementById("botonlocalidad").innerHTML = "Comodoro Rivadavia"
+}
+
+document.getElementById("email").onclick = function () {
+     document.getElementById("botonemail").innerHTML = "Zfegea@hotmail.com"
+     }
+
+     document.getElementById("telefono").onclick = function () {
+          document.getElementById("botontelefono").innerHTML = "2974335735"
+          }     
